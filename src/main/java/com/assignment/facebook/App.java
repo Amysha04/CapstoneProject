f72e3d5f127eacddeecd3d790be11100896a416d
Amrita <ranoook91@gmail.com>
2024-10-14 23:11:30 -0500

    More changes

--- src/main/java/com/assignment/facebook/App.java
+++ src/main/java/com/assignment/facebook/App.java
@@ -33,8 +33,8 @@ public class App {
         WebElement submitButton = driver.findElement(By.cssSelector("button"));
         submitButton.click();      	
         
-        WebElement message = driver.findElement(By.className("_9ay7"));
-        message.getText();
+       // WebElement message = driver.findElement(By.className("_9ay7"));
+        //message.getText();
 
         driver.quit();
     }
